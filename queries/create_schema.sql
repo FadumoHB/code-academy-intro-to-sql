8b8a34cf8ecf2cd19c9530087d379daba7802292
FadumoHB <fhbotan1@gmail.com>
2021-09-13 16:33:05 +0000

    commit 2

--- queries/create_schema.sql
+++ queries/create_schema.sql
@@ -21,4 +21,84 @@ SELECT customer_name, city FROM codeacademy.customer WHERE city!= 'Birmingham';
 
 SELECT order_id FROM codeacademy.order WHERE order_date > '2019-05-31';
 
- 
+
+
+SELECT A.customer_name
+,B.total_amount
+FROM codeacademy.customer A
+INNER JOIN codeacademy.order B
+ON A.customer_id = B.customer_id 
+LIMIT 10;
+
+
+
+SELECT A.customer_name
+,B.total_amount, order_date 
+FROM codeacademy.customer A
+INNER JOIN codeacademy.order B
+ON A.customer_id = B.customer_id 
+LIMIT 10;
+
+
+SELECT A.customer_name
+,B.total_amount, order_date
+FROM codecademy.customer A 
+LEFT JOIN codecademy.order B 
+ON A.customer_id = B.customer_id;
+
+
+
+SELECT
+A.city,
+count (B.order_id)
+FROM codeacademy.customer A 
+INNER JOIN codeacademy.order B 
+ON A.customer_id = B.customer_id 
+GROUP BY A.city;
+
+
+SELECT
+A.city,
+count (B.order_id)
+FROM codeacademy.customer A 
+INNER JOIN codeacademy.order B 
+ON A.customer_id = B.customer_id 
+GROUP BY A.city;
+
+SELECT A.customer_name, MIN (B.order_date) FROM codeacademy.customer A 
+INNER JOIN codeacademy.order B ON A.customer_id = B.customer_id
+GROUP BY A.customer_name;
+
+SELECT A.customer_name, MAX (B.order_date) FROM codeacademy.customer A 
+INNER JOIN codeacademy.order B ON A.customer_id = B.customer_id
+GROUP BY A.customer_name;
+
+
+SELECT A.customer_name, SUM (B.total_amount) FROM codeacademy.customer A 
+LEFT JOIN codeacademy.order B ON A.customer_id = B.customer_id
+GROUP BY A.customer_name;
+
+
+SELECT LEFT(order_date::text,7) YEAR_MONTH, SUM (total_amount) TOTAL 
+FROM codeacademy.order 
+GROUP BY YEAR_MONTH
+ORDER BY TOTAL DESC ;
+
+
+SELECT LEFT(A.order_date::text,7) year_month,
+SUM(A.total_amount), B.product_name
+FROM codeacademy.order_line C 
+INNER JOIN codeacademy.product B ON C.product_id = B.product_id 
+INNER JOIN codeacademy.order A ON
+C.order_id = A.order_id GROUP BY year_month, B.product_name;
+
+SELECT A.customer_name, B.order_date, B.total_amount
+FROM codeacademy.customer A 
+INNER JOIN codeacademy.order B
+ON B.customer_id = A.customer_id 
+WHERE B.order_status_id=2;
+
+
+
+
+
